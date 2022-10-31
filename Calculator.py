7cbf0ad16623d2c1cde1d7ca3529c21acfc85ac6
Jian Wei Ng <s10208269@connect.np.edu.sg>
2022-11-01 02:07:34 +0800

    Implement Trigonometry Code

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,4 @@
+import math
 
 def add(a, b):
     return a + b
@@ -11,30 +12,71 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
-print("Please select the type of operations: \n")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("3. Divide")
 
+print("Please select the type of operations")
+print("1. Standard Arithmetic")
+print("2. Trigonometry ")
+#print("3. ")
+#print("4. ")
 while True:
-    option = int(input("Enter number of choice: "))
-
+    option = int(input("Enter type of operations: "))
     if option in (1,2,3,4):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
+        #Option 1: Arithmetic
         if option == 1:
-            print(num1, "+", num2, "=", add(num1, num2))
+            print("Please select the type of Arithmetic")
+            print("1. Add")
+            print("2. Subtract")
+            print("3. Multiply")
+            print("3. Divide")
+            choice = int(input("Enter type of Arithmetic: "))
+            if choice in (1,2,3,4):
+                num1 = float(input("Enter first number: "))
+                num2 = float(input("Enter second number: "))
+                if choice == 1:
+                    print(num1, "+", num2, "=", add(num1, num2))
 
+                elif choice == 2:
+                    print(num1, "-", num2, "=", subtract(num1, num2))
+
+                elif choice == 3:
+                    print(num1, "*", num2, "=", multiply(num1, num2))
+
+                elif choice == 4:
+                    print(num1, "/", num2, "=", divide(num1, num2))
+                    
+        #Option 2: Trigonometry        
         elif option == 2:
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif option == 3:
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif option == 4:
-            print(num1, "/", num2, "=", divide(num1, num2))
-
+            print("Please select the type of Trigonometry")
+            print("1. Sin")
+            print("2. Cos")
+            print("3. Tan")
+            print("4. Inversed Sin")
+            print("5. Inversed Cos")
+            print("6. Inversed Tan")
+            choice = int(input("Enter type of Trigonometry: "))
+            if choice == 1:
+                num = float(input("Enter radians number: ")) 
+                print(math.sin(num))
+            if choice == 2:
+                num = float(input("Enter radians number: ")) 
+                print(math.tan(num))
+            if choice == 3:
+                num = float(input("Enter radians number: ")) 
+                print(math.cos(num))
+            if choice == 4:
+                num = float(input("Enter radians number: ")) 
+                print(math.asin(num))
+            if choice == 5:
+                num = float(input("Enter radians number: ")) 
+                print(math.acos(num))
+            if choice == 6:
+                num = float(input("Enter radians number: ")) 
+                print(math.atan(num))
+        #Option 3: 
+        #Option 4:  
     else:
         print("Invalid input, please try again.")
+        
+    
+
+
