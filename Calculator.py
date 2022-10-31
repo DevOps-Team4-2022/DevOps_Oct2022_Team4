32bf7f77fc9a6acd555331b86b94d4dc75b31974
Jian Wei Ng <s10208269@connect.np.edu.sg>
2022-11-01 01:42:07 +0800

    Implement code to prompt user for input and display calculation

--- Calculator.py
+++ Calculator.py
@@ -11,4 +11,30 @@ def multiply(a, b):
 def divide(a, b):
     return a / b
 
+print("Please select the type of operations: \n")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("3. Divide")
 
+while True:
+    option = int(input("Enter number of choice: "))
+
+    if option in (1,2,3,4):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if option == 1:
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif option == 2:
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif option == 3:
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif option == 4:
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+    else:
+        print("Invalid input, please try again.")
